e8c26ecd815c0f6871c4febe076eb0c52e4440bf
Zach Mikaya <zmikaya@mit.edu>
2016-08-03 06:42:35 +0000

    Version upgrade to 1.3.3 then to 1.3.5

--- server/methods/geocoding.js
+++ server/methods/geocoding.js
@@ -3,7 +3,7 @@ function initializeGeocoding() {
     // if (Meteor.settings['private'] == undefined) {
     //     throw new Meteor.Error('no-settings', "No configuration found. Did you forget to run meteor with --settings option?");
     // }
-    var apiKey = Meteor.settings['private'].google_api_key;
+    // var apiKey = Meteor.settings['private'].google_api_key;
     return new GeoCoder({
         geocoderProvider: "google",
         httpAdapter: "https",
