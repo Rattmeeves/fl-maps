d58e4a4301d2bbe2da06c3511697e13bb074de9c
Zach Mikaya <zmikaya@mit.edu>
2016-08-03 06:10:42 +0000

    Prevent error for setting object access.

--- server/methods/geocoding.js
+++ server/methods/geocoding.js
@@ -1,12 +1,13 @@
 function initializeGeocoding() {
-    if (Meteor.settings['private'] == undefined) {
-        throw new Meteor.Error('no-settings', "No configuration found. Did you forget to run meteor with --settings option?");
-    }
+    // TODO: Uncommnet lines for deployment. We need the settings.json file.
+    // if (Meteor.settings['private'] == undefined) {
+    //     throw new Meteor.Error('no-settings', "No configuration found. Did you forget to run meteor with --settings option?");
+    // }
     var apiKey = Meteor.settings['private'].google_api_key;
     return new GeoCoder({
         geocoderProvider: "google",
         httpAdapter: "https",
-        apiKey: apiKey
+        // apiKey: apiKey
     });
 }
 geoCoder = initializeGeocoding();
