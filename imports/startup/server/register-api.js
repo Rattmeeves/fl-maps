a9f125a3f3362e6990dd99aa7c89dcbb7702910f
Zach Mikaya <zmikaya@mit.edu>
2016-08-03 08:55:02 +0000

    Refactored rss publications.

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,3 +2,4 @@ import '../../api/categories/server/publications.js';
 import '../../api/events/methods.js';
 import '../../api/events/server/publications.js';
 import '../../api/geocoding/methods.js';
+import '../../api/rss/server/publications.js';
